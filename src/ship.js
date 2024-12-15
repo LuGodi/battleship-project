0becc7244ecc47a803282572d2fd38d737ad5c04
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2024-12-15 09:05:23 -0300

    Add ship class

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,14 @@
 export default class Ship {
-  constructor() {}
+  timesHit = 0;
+  constructor(length) {
+    this.length = length;
+    this.sunk = false;
+    Object.seal(this);
+  }
+  hit() {
+    this.timesHit++;
+  }
+  isSunk() {
+    return this.timesHit >= this.length;
+  }
 }
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -4,4 +4,36 @@ describe("ship", () => {
   test("should not be undefined", () => {
     expect(new Ship()).toBeDefined();
   });
+  test("should be instance of ship", () => {
+    expect(new Ship()).toBeInstanceOf(Ship);
+  });
+  describe("ships properties", () => {
+    const myShip = new Ship(5);
+    test("should have length attribute", () => {
+      expect(myShip).toHaveProperty("length");
+      expect(myShip.length).toBe(5);
+    });
+    test("should have sunk property", () => {
+      expect(myShip).toHaveProperty("sunk");
+    });
+    test("sunk property should be a boolean", () => {
+      expect(typeof myShip.sunk).toBe("boolean");
+    });
+    test("should have timesHit property", () => {
+      expect(myShip).toHaveProperty("timesHit");
+    });
+    test("should have hit method that increases number of times hit", () => {
+      console.log(myShip);
+      expect(myShip.timesHit).toBe(0);
+      myShip.hit();
+      expect(myShip.timesHit).toBe(1);
+    });
+    test("should be sunk if number of hits is equal or higher than timesHit", () => {
+      myShip.timesHit = 4;
+      expect(myShip.isSunk()).toBe(false);
+      myShip.hit();
+      expect(myShip.isSunk()).toBe(true);
+    });
+    test.todo("should be sealed");
+  });
 });
