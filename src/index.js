20dac4c13c741cfda6c7503e1169803added433e
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2025-01-12 17:00:46 -0300

    Css clean up

--- src/index.js
+++ src/index.js
@@ -9,4 +9,4 @@ console.log(mainContainer);
 const currentPlayerBoard = new Board();
 const enemyPlayerBoard = new Board();
 mainContainer.append(currentPlayerBoard.init(10, 10, "board-container"));
-// mainContainer.append(enemyPlayerBoard.init(10, 10, "board-container"));
+mainContainer.append(enemyPlayerBoard.init(10, 10, "board-container"));
