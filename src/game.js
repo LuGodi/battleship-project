18a99f075ab7aef37072c3a020353d2b440bff6c
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2025-01-16 15:36:34 -0300

    Implement switching player ability

--- src/game.js
+++ src/game.js
@@ -4,21 +4,26 @@ import Player from "./player";
 //this will serve as the mediator
 export default class Game {
   static players = [];
+  static player1;
+  static player2;
   static currentPlayer;
   static start() {
     //populate the gameboard with predetermined coordinates
     console.log("start game");
-    const player1 = new Player("real", "p1");
-    const player2 = new Player("real", "p2");
-    Game.players.push(player1, player2);
+    this.player1 = new Player("real", "p1");
+    this.player2 = new Player("real", "p2");
+    // Game.players.push(player1, player2);
 
-    Game.currentPlayer = player1;
+    Game.currentPlayer = this.player1;
   }
   static getCurrentPlayer() {
-    console.log(Game.currentPlayer);
-    return Game.currentPlayer.name;
+    console.log(Game.currentPlayer.name);
+    return Game.currentPlayer;
   }
-
+  static switchPlayer() {
+    this.currentPlayer =
+      this.currentPlayer === player1 ? this.player2 : this.player1;
+  }
   static populatePredetermined(player) {
     console.log("populating player", player.name);
     player.gameboard.placeShip("A", 1, 2, "horizontal");
